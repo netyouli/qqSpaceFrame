225701fb41775879fdd20966919866efb3ef40fe
netyouli <712641411@qq.com>
2015-05-12 14:22:00 +0800

    Initial Commit

---
+++ qqSpaceFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qqSpaceFrame
+//
+//  Created by 吴海超 on 15/5/12.
+//  Copyright (c) 2015年 吴海超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
